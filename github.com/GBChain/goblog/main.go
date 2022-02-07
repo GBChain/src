686a932c7c1efe1d39672d277af06384d76a1b91
一博 <3364114553@qq.com>
2022-02-07 23:49:11 +0800

    保存博文到数据库

--- github.com/GBChain/goblog/main.go
+++ github.com/GBChain/goblog/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
@@ -23,9 +24,9 @@ func initDB() {
 
 	var err error
 	config := mysql.Config{
-		User:                 "homestead",
-		Passwd:               "secret",
-		Addr:                 "127.0.0.1:33060",
+		User:                 "root",
+		Passwd:               "QQqq4213801",
+		Addr:                 "127.0.0.1:3306",
 		Net:                  "tcp",
 		DBName:               "goblog",
 		AllowNativePasswords: true,
@@ -62,6 +63,40 @@ func createTables() {
 
 	_, err := db.Exec(createArticlesSQL)
 	checkError(err)
+}
+
+func saveArticleToDB(title string, body string) (int64, error) {
+
+	// 变量初始化
+	var (
+		id   int64
+		err  error
+		rs   sql.Result
+		stmt *sql.Stmt
+	)
+
+	// 1. 获取一个 prepare 声明语句
+	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES(?,?)")
+	// 例行的错误检测
+	if err != nil {
+		return 0, err
+	}
+
+	// 2. 在此函数运行结束后关闭此语句，防止占用 SQL 连接
+	defer stmt.Close()
+
+	// 3. 执行请求，传参进入绑定的内容
+	rs, err = stmt.Exec(title, body)
+	if err != nil {
+		return 0, err
+	}
+
+	// 4. 插入成功的话，会返回自增 ID
+	if id, err = rs.LastInsertId(); id > 0 {
+		return id, nil
+	}
+
+	return 0, err
 }
 
 // ArticlesFormData 创建博文表单数据
@@ -121,11 +156,21 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 检查是否有错误
 	if len(errors) == 0 {
-		fmt.Fprint(w, "验证通过!<br>")
-		fmt.Fprintf(w, "title 的值为: %v <br>", title)
-		fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
-		fmt.Fprintf(w, "body 的值为: %v <br>", body)
-		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
+
+		lastInsertID, err := saveArticleToDB(title, body)
+		if lastInsertID > 0 {
+			fmt.Fprint(w, "插入成功，ID 为"+strconv.FormatInt(lastInsertID, 10))
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
+
+		// fmt.Fprint(w, "验证通过!<br>")
+		// fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		// fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
+		// fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		// fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
 	} else {
 
 		storeURL, _ := router.Get("articles.store").URL()
